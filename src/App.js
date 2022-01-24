cfd8d70a04e147c524387151bc9099921a2bde2e
Babar Baig <BobBaig@outlook.com>
2022-01-24 00:45:41 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useFormik} from 'formik'
+import {FormikProvider, useFormik} from 'formik'
 
 function App() {
   const formik = useFormik({
@@ -10,6 +10,13 @@ function App() {
     },
     onSubmit: values => {
       console.log('form: ', values)
+    },
+    validate: values => {
+      let errors = {}
+      if (!values.name)  errors.name  = 'Required'
+      if (!values.email) errors.email = 'Required'
+      if (!values.pswd)  errors.pswd  = 'Required'
+      return errors
     }
   })
 
@@ -18,10 +25,13 @@ function App() {
       <form onSubmit={formik.handleSubmit}>
         <div>Name</div>
         <input name="name"  type="text" onChange={formik.handleChange} value={formik.values.name} />
+        {formik.errors.name  ? <div style={{color:'red'}}>{formik.errors.name}</div>: null}
         <div>Email</div>
         <input name="email" type="text" onChange={formik.handleChange} value={formik.values.email}/>
+        {formik.errors.email ? <div style={{color:'red'}}>{formik.errors.email}</div>: null}
         <div>Password</div>
         <input name="pswd"  type="text" onChange={formik.handleChange} value={formik.values.pswd} />
+        {formik.errors.pswd  ? <div style={{color:'red'}}>{formik.errors.pswd}</div>: null}
         <button type="submit">Submit</button>
       </form>
     </div>
